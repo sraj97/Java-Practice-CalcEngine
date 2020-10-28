491784aab9045a38af84d40044ce9a2cf6a8a1fe
sraj97 <shwetarajput0702@gmail.com>
2020-10-28 08:53:11 +0000

    Programme refactored to include classes

--- src/com/rajputs/calcengine/Main.java
+++ src/com/rajputs/calcengine/Main.java
@@ -6,140 +6,31 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        double[] leftVals = {100.0, 25.0, 225.0, 11.0};
-        double[] rightVals = {50.0, 92.0, 17.0, 3.0};
-        char[] opCodes = {'d', 'a', 's', 'm'};
-        double[] results = new double[opCodes.length];
+        performCalculations();
+    }
 
-        if(args.length == 0) {
-            for (int i = 0; i < opCodes.length; i++) {
-                results[i] = execute(opCodes[i], leftVals[i], rightVals[i]);
-            }
-            for (double currentResult : results)
-                System.out.println(currentResult);
-        } else if (args.length == 1 && args[0].equals("interactive")){
-            executeInteractively();
-        } else if(args.length == 3){
-            handleCommandLine(args);
-        } else {
-            System.out.println("Please provide an operation code and 2 numeric values");
+    static void performCalculations() {
+
+        MathEquation[] equations = new MathEquation[4];
+        equations[0] = create(100, 50,  'd');
+        equations[1] = create(25, 92, 'a');
+        equations[2] = create(225, 17, 's');
+        equations[3] = create(11, 3, 'm');
+
+        for (MathEquation equation: equations) {
+            equation.execute();
+            System.out.println("result = " + equation.result);
         }
-
     }
 
-    static void executeInteractively(){
-        System.out.println("Enter an operation and two numbers");
-        Scanner scanner = new Scanner(System.in);
-        String userInput = scanner.nextLine();
-        String[] parts = userInput.split(" ");
-        performOperation(parts);
+    private static MathEquation create(double leftVal, double rightVal, char opCode){
+        MathEquation equation = new MathEquation();
+        equation.leftVal = leftVal;
+        equation.rightVal = rightVal;
+        equation.opCode = opCode;
+        return equation;
     }
-
-    private static void performOperation(String[] parts) {
-        char opCode = opCodeFromString(parts[0]);
-        if (opCode == 'w'){
-            handleWhen(parts);
-        } else {
-            double leftVal = valueFromWord(parts[1]);
-            double rightVal = valueFromWord(parts[2]);
-            double result = execute(opCode, leftVal, rightVal);
-            displayResult(opCode, leftVal, rightVal, result);
-        }
-
-    }
-
-    private static void handleWhen(String[] parts) {
-        LocalDate startDate = LocalDate.parse(parts[1]);
-        long daysToAdd = (long) valueFromWord(parts[2]);
-        LocalDate newDate = startDate.plusDays(daysToAdd);
-        String output = String.format("%s plus %d days is %s", startDate, daysToAdd, newDate);
-        System.out.println(output);
-    }
-
-
-    private static void displayResult(char opCode, double leftVal, double rightVal, double result) {
-        char symbol = symbolFromOpCode(opCode);
-
-//        StringBuilder builder = new StringBuilder(20);
-//        builder.append(leftVal);
-//        builder.append(" ");
-//        builder.append(symbol);
-//        builder.append(" ");
-//        builder.append(rightVal);
-//        builder.append(" = ");
-//        builder.append(result);
-//        String output = builder.toString();
-
-        String output = String.format("%.3f %c %.3f = %.3f", leftVal, symbol, rightVal, result);
-        System.out.println(output);
-    }
-
-    private static char symbolFromOpCode (char opCode){
-        char[] opCodes = {'a', 's', 'm', 'd'};
-        char[] symbols = {'+', '-', '*', '/'};
-
-        char symbol = ' ';
-        for (int i = 0; i < opCodes.length; i++) {
-            if(opCode == opCodes[i]){
-                symbol = symbols[i];
-                break;
-            }
-        }
-        return symbol;
-    }
-    private static void handleCommandLine(String[] args) {
-        char opCode =  args[0].charAt(0);
-        double leftVal = Double.parseDouble(args[1]);
-        double rightVal = Double.parseDouble(args[2]);
-        double result = execute(opCode, leftVal, rightVal);
-        System.out.println(result);
-    }
-
-    static double execute(char opCode, double leftVal, double rightVal){
-        double result;
-        switch (opCode) {
-            case 'a':
-                result = leftVal + rightVal;
-                break;
-            case 's':
-                result = leftVal - rightVal;
-                break;
-            case 'm':
-                result = leftVal * rightVal;
-                break;
-            case 'd':
-                result = rightVal != 0 ? leftVal / rightVal : 0.0;
-                break;
-            default:
-                result = 0.0;
-                System.out.println("Invalid opCode " + opCode);
-                break;
-        }
-        return result;
-    }
-
-    static char opCodeFromString(String operationName) {
-        char opCode = operationName.charAt(0);
-        return opCode;
-    }
-
-    static double valueFromWord(String word){
-        String[] numberWords = {
-                "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
-        };
-        double value = -1;
-        for (int i = 0; i < numberWords.length; i++) {
-            if(word.equals(numberWords[i])){
-                value = i;
-                break;
-            }
-        }
-        if (value == -1){
-            value = Double.parseDouble(word);
-        }
-        return value;
-    }
-
 
 }
+
 
