12fc8cc23ccab1229305467bf952b90b84444821
sraj97 <shwetarajput0702@gmail.com>
2020-11-03 09:49:55 +0000

    Programme refactored to include inheritance

--- src/com/rajputs/calcengine/Main.java
+++ src/com/rajputs/calcengine/Main.java
@@ -1,35 +1,53 @@
 package com.rajputs.calcengine;
 
-import java.time.LocalDate;
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
-        performCalculations();
+        // performCalculations();
+        Divider divider = new Divider();
+        doCalculation(divider, 100, 50);
+        Adder adder = new Adder();
+        doCalculation(adder, 25, 92);
+    }
+
+    static void doCalculation(CalculateBase calculation, double leftVal, double rightVal){
+        calculation.setLeftVal(leftVal);
+        calculation.setRightVal(rightVal);
+        calculation.calculate();
+        System.out.println("Calculation result = " + calculation.getResult());
     }
 
     static void performCalculations() {
 
         MathEquation[] equations = new MathEquation[4];
-        equations[0] = create(100, 50,  'd');
-        equations[1] = create(25, 92, 'a');
-        equations[2] = create(225, 17, 's');
-        equations[3] = create(11, 3, 'm');
+        equations[0] = new MathEquation( 'd', 100, 50);
+        equations[1] = new MathEquation('a', 25, 92);
+        equations[2] = new MathEquation('s', 225, 17);
+        equations[3] = new MathEquation('m', 11, 3);
 
         for (MathEquation equation: equations) {
             equation.execute();
-            System.out.println("result = " + equation.result);
+            System.out.println("result = " + equation.getResult());
         }
+
+        System.out.println("Average result = " + MathEquation.getAverageResult());
+
+        MathEquation equationOverload = new MathEquation('d');
+
+        double leftDouble = 9;
+        double rightDouble = 4;
+        equationOverload.execute(leftDouble, rightDouble);
+        System.out.println("Overloaded result with doubles: " + equationOverload.getResult());
+
+
+        int leftInt = 9;
+        int rightInt = 4;
+
+        equationOverload.execute(leftInt, rightInt);
+        System.out.println("Overloaded result with ints : " + equationOverload.getResult());
     }
 
-    private static MathEquation create(double leftVal, double rightVal, char opCode){
-        MathEquation equation = new MathEquation();
-        equation.leftVal = leftVal;
-        equation.rightVal = rightVal;
-        equation.opCode = opCode;
-        return equation;
-    }
+
 
 }
 
--- src/com/rajputs/calcengine/MathEquation.java
+++ src/com/rajputs/calcengine/MathEquation.java
@@ -1,12 +1,26 @@
 package com.rajputs.calcengine;
 
 public class MathEquation {
-    double leftVal;
-    double rightVal;
-    char opCode;
-    double result;
+    private double leftVal;
+    private double rightVal;
+    private char opCode;
+    private double result;
 
-    void execute() {
+    private static int numberOfCalculations;
+    private static double sumOfResults;
+
+    public MathEquation(){}
+    public MathEquation(char opCode){
+       this.opCode = opCode;
+    }
+
+   public MathEquation(char opCode, double leftVal, double rightVal){
+       this(opCode);
+       this.leftVal = leftVal;
+       this.rightVal = rightVal;
+   }
+
+   public void execute() {
         switch (opCode) {
             case 'a':
                 result = leftVal + rightVal;
@@ -25,8 +39,57 @@ public class MathEquation {
                 System.out.println("Invalid opCode " + opCode);
                 break;
         }
+
+        numberOfCalculations++;
+        sumOfResults += result;
     }
 
+    public void execute(double leftVal, double rightVal){
+        this.leftVal = leftVal;
+        this.rightVal = rightVal;
+
+        execute();
+    }
+
+    public void execute(int leftVal, int rightVal){
+        this.leftVal = leftVal;
+        this.rightVal = rightVal;
+        execute();
+
+        result = (int) result;
+    }
+
+    public static double getAverageResult(){
+        return sumOfResults / numberOfCalculations;
+    }
+
+    public double getLeftVal(){
+        return leftVal;
+    }
+
+    public void setLeftVal(double leftVal){
+        this.leftVal = leftVal;
+    }
+
+    public double getRightVal(){
+        return rightVal;
+    }
+
+    public void setRightVal(double rightVal){
+        this.rightVal = rightVal;
+    }
+
+    public char getOpCode(){
+        return opCode;
+    }
+
+    public void setOpCode(char opCode){
+        this.opCode = opCode;
+    }
+
+    public double getResult() {
+        return result;
+    }
 
 
 }
